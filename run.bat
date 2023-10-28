4486fb8a6bded1e6c18c8951d1a8661b93adcd2c
Leonard Meagher <leonardmeagher2@gmail.com>
2023-10-28 09:18:13 -0700

    had to reset my token

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd /D "%~dp0"
-set DISCORD_TOKEN=MTE2NjcyNDA2MDQwNzkzNDk3Ng.G3FL3i.QUlTOPD5F9nNubjkbWuRivhWhZF5Hv0Gk4gpXQ
+set DISCORD_TOKEN=
 call git pull
 (call conda deactivate && call conda deactivate && call conda deactivate) 2>nul
 call conda activate oobabot
